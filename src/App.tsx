53fd418e4777711acccb0eef5ff6cdf37784e23e
rajeev1209 <kumar.rajeevranjan10@yahoo.com>
2023-12-21 22:33:13 -0500

    Forntend-UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,21 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { HomePage } from './layouts/HomePage/HomePage';
+<<<<<<< Updated upstream
+=======
+import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage';
+>>>>>>> Stashed changes
 
 export const App = () => {
   return (
     <div>
       <Navbar />
+<<<<<<< Updated upstream
       <HomePage />
+=======
+      {/* <HomePage /> */}
+      <SearchBookPage />
+>>>>>>> Stashed changes
       <Footer />
     </div>
   );
